42e401ec5066ed9090f635ca5f6765b60f7f91bb
bill080307 <bill080307@live.cn>
2019-08-01 02:06:11 +0800

    更新固定器

--- tool/pin-server/pin-manage.py
+++ tool/pin-server/pin-manage.py
@@ -38,6 +38,23 @@ def getmyhash():
             data = loadjson(os.path.join(path, item))
             for k1, v1 in data.items():
                 mylist.append(v1['cid'])
+        if item == 'videoshare.json':
+            data = loadjson(os.path.join(path, item))
+            for k1, v1 in data['template'].items():
+                mylist.append(v1)
+            for k1, v1 in data['users'].items():
+                h = api.name.resolve(v1)
+                mylist.append(h['Path'][6:])
+                t = json.loads(api.cat("%s/user.json" % h['Path']))
+                mylist.append(t['avatar'][6:])
+                for v2 in t['type']:
+                    t2 = json.loads(api.cat("%s/%s.json" % (h['Path'], v2['name'])))
+                    for v3 in t2:
+                        mylist.append(v3['url'][6:])
+                        t3 = json.loads(api.cat("%s/files.json" % v3['url']))
+                        mylist.append(t3['cover'][6:])
+                        for v4 in t3['files']:
+                            mylist.append(v4['url'][6:])
     return mylist
 
 
@@ -62,10 +79,13 @@ if __name__ == '__main__':
 
     rmpinhash = list(set(localhash).difference(set(pinhash)))
     downloadpinhash = list(set(pinhash).difference(set(localhash)))
-
+    print(u'已经固定的')
     print(localhash)
+    print(u'需要固定的')
     print(pinhash)
+    print(u'需要删除固定的')
     print(rmpinhash)
     ipfsrmpin(rmpinhash)
+    print(u'需要添加固定的')
     print(downloadpinhash)
     ipfsaddpin(downloadpinhash)

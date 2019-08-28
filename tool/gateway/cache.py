027f7ad7d1585c9b15a4e417fc1ded83d41a6e97
bill080307 <bill080307@live.cn>
2019-08-29 00:47:20 +0800

    更新

---
+++ tool/gateway/cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import ipfshttpclient
+import json
+import os
+import redis
+
+api = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001', timeout=1200)
+path = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'data')
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+
+def loadjson(jsonfile):
+    with open(jsonfile) as json_file:
+        data = json.load(json_file)
+        return data
+
+
+if __name__ == '__main__':
+    files = os.listdir(path)
+    for item in files:
+        if item == 'videoshare.json':
+            data = loadjson(os.path.join(path, item))
+            for k1, v1 in data['users'].items():
+                h = api.name.resolve(v1)
+                r.set(v1, h['Path'][6:])
+
--- tool/pin-server/pin-server.py
+++ tool/pin-server/pin-server.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python3
 import ipfshttpclient
 import pika
+import time
+
 username = 'guest'
 pwd = 'guest'
 api = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001', timeout=1200)
 user_pwd = pika.PlainCredentials(username, pwd)
-s_conn = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1', credentials=user_pwd))
-chan = s_conn.channel()
-chan.queue_declare(queue='ipfspin', durable=True)
 
 
 def callback(ch, method, properties, body):
     print(body.decode('UTF-8'))
-    api.pin.add(body.decode('UTF-8'))
+    try:
+        api.pin.add(body.decode('UTF-8'))
+    except:
+        print("pin error")
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-chan.basic_qos(prefetch_count=1)
-chan.basic_consume(queue='ipfspin', on_message_callback=callback, auto_ack=False)
-chan.start_consuming()
+while True:
+    try:
+        s_conn = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1', credentials=user_pwd))
+        chan = s_conn.channel()
+        chan.queue_declare(queue='ipfspin', durable=True)
+
+        chan.basic_qos(prefetch_count=1)
+        chan.basic_consume(queue='ipfspin', on_message_callback=callback, auto_ack=False)
+        chan.start_consuming()
+    except:
+        time.sleep(10)
+
+# chan.basic_qos(prefetch_count=1)
+# chan.basic_consume(queue='ipfspin', on_message_callback=callback, auto_ack=False)
+# while True:
+#    try:
+#        chan.start_consuming()
+#    except:
+#        time.sleep(10)
